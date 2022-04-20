aab09f8323245e19b9ec6efeea37e69f6020f648
AdanPomboTorres <adanpombotorres@gmail.com>
2022-04-20 08:39:41 +0200

    commit

---
+++ calculadora.test.js
@@ -0,0 +1,17 @@
+const { test } = require("@jest/globals");
+
+
+ describe("funcion uno", () => {
+    test("Comprobación de uno", () => {
+         expect(true).toBe(true);
+    });
+
+    // test("recibe parametros", () =>{
+    //     test("Comprobación de fizz", () => {
+    //         expect(fizz(2)).toBe(2);
+    //     });
+    
+    //  test("1.- clearReturns buzz if its multiple of 3", () => {
+    //      expect(saludarFn("Antonio")).toBe("Hola Antonio");
+    //  });
+}); le
